32468268275ed23343192f83dab0b5593b574445
Sevinj-Joy <seva.rasul15@gmail.com>
2021-12-07 20:57:28 +0400

    Javascript tasks updated

--- Works/Javascript_task/ad.js
+++ Works/Javascript_task/ad.js
@@ -1,9 +1,53 @@
-Name = prompt("Name:")
-Surname =prompt ("Surname:")
+// task 1 // Name = prompt("Name:")
+// Surname =prompt ("Surname:")
 
-if (Name == "Memmed" && Surname == "Hesenov") {
-    alert("Memmed Hesenov sen xos gelmisen")
-}
-else{
-    alert("Melumatlar duzgun daxil edilmeyib")
-}
+// if (Name == "Memmed" && Surname == "Hesenov") {
+//     alert("Memmed Hesenov sen xos gelmisen")
+// }
+// else{
+//     alert("Melumatlar duzgun daxil edilmeyib")
+// }
+
+// 3+(2-true+false)/3&&true complex ifadəsinin nəticəsi nə olacaq və bunun hansı sıralamalı ilə həllediləcəyi kod yazaraq izah edin
+//  netice true
+
+// 3=='3'==='3' 
+// let Num= 3;
+// let three='3';
+
+// console.log(Num==three);
+// 3=="3" true
+// 1==='3' false
+
+// Username = prompt("name:")
+// Pword = prompt("password:")
+
+// if(Username =="admin" && Pword =="123456") {
+//     alert( 'Tebrikler siz sisteme daxil oldugunuz')
+// }
+// else{
+//     alert('Deyerler bos ola bilmez')
+// }
+// for (let i =1; i<1000; i++) {
+//     console.log(i)
+// }
+
+// var say=prompt(" reqem yazin");
+// if(say % 2==0)
+// {
+// alert("Yazilan "+ say + " reqemi cutdur")
+// }
+// else
+// {
+// alert("Yazilan "+ say + " reqemi tekdir")
+// }
+
+// var say=prompt(" reqem yazin");
+// if(say % 3==1)
+// {
+// alert("Yazilan "+ say + " reqemi 3e bolunur")
+// }
+// else
+// {
+// alert("Teessuf "+ say + " 3e bolunmur")
+// }
