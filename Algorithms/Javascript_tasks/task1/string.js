47d5e047bdd8326f020af02db4df90beae3cc0df
Sevinj-Joy <seva.rasul15@gmail.com>
2021-12-13 21:03:03 +0400

    Javascrip task 2 updated

---
+++ Algorithms/Javascript_tasks/task1/string.js
@@ -0,0 +1,9 @@
+// let x = myFunction(4);   
+
+// function myFunction(a) {
+//   alert( a * a )          
+// }
+let a =5
+function fun(x = a * a) {  
+    prompt ("This is a prompt box", + x);  
+    }  
