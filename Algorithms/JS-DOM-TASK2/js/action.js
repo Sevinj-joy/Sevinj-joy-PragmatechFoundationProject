47073f6499e4a9736117f9b4176f6a80c2ab70c8
Sevinj-Joy <seva.rasul15@gmail.com>
2021-12-20 20:58:28 +0400

    JS-DOM- TASK UPDATED

--- Algorithms/JS-DOM-TASK2/js/action.js
+++ Algorithms/JS-DOM-TASK2/js/action.js
@@ -1,24 +1,35 @@
-
+// // EMR 1 DUYMESINI BASANDA
 function backrndcolor(){
     document.body.style.backgroundColor = "lightgray";
 }
+// EMR 2 DUYMESINI BASANDA
 function headcolor(){
     document.getElementById("heading").style.backgroundColor = "red";
 
 }
+// EMR 3 DUYMESINI BASANDA
 function textcolor(){
     document.getElementById("ptext").style.backgroundColor = "yellow";
 }
  cntText = "Proqramlaşdırma sahəsində bilik və təcrübə istifadə müddəti çox tez bitən məfhumdur.Bu gün əldə etdiyiniz bilik bir neçə il sonra istifadəyə yararsız hala gələbilər. Buna görə də sizin bir vəzifəniz də öz bilik portfolionuzu daimi yeniləmək olmalıdır.Bunun üçün kitabda verilən tövsiyələr"
  
-
+//  EMR 4 DUYMESINI BASANDA
  function countext(){
     say = cntText.length;
     alert(say);
  }
+//  EMR 5 DUYMESINI BASANDA
 
  function WordCount() { 
     sozsayi = cntText.split(" ").length ;
     alert(sozsayi);
 
-  }
+}
+//   EMR 6 DUYMESINI BASANDA
+let text = document.getElementById("text");
+sozsayi = text.split("proqramçı").length;
+
+function soztap(){
+    alert(sozsayi);
+    
+}
