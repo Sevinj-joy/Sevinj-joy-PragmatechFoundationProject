2f4b8a1079c999fb094ad1a46e8ba77b41c6c29a
Sevinj-Joy <seva.rasul15@gmail.com>
2021-12-27 19:18:49 +0400

    form JS updated

--- Algorithms/Form/JS/form.js
+++ Algorithms/Form/JS/form.js
@@ -1,8 +1,19 @@
 
 function submit(){
 
-    alert(document.getElementsByTagName("input").innerHTML= "Melumatlar bos qoyula bilmez")
-    
+
+//    let nomre = document.getElementById("phone-number").value
+//     alert("+994 "+ nomre)
+
+
+    // let x = document.getElementById("start").value
+    // if(document.getElementById("start").value == "2006")
+    // alert("siz daxil ola bilmersiz")
+
+
+    // alert(document.getElementsByTagName("input").innerHTML= "Melumatlar bos qoyula bilmez")
+
+
 // if (x = "sevinc"){
 //     document.getElementsByTagName("input").innerHTML = x
 // }
