a38b7e7b8c77117471c4ce7cadb1206aedb86c57
Sevinj-Joy <seva.rasul15@gmail.com>
2021-12-27 18:29:01 +0400

    form JS updated

---
+++ Algorithms/Form/JS/form.js
@@ -0,0 +1,10 @@
+
+function submit(){
+
+    alert(document.getElementsByTagName("input").innerHTML= "Melumatlar bos qoyula bilmez")
+    
+// if (x = "sevinc"){
+//     document.getElementsByTagName("input").innerHTML = x
+// }
+// alert(x + " "+ "xos gelmisen")
+ }
