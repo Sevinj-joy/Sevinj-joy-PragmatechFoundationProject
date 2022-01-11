fd3ad3a99f710b7ebf64afeb4fc34ef214a0f506
Sevinj-Joy <seva.rasul15@gmail.com>
2022-01-11 20:41:47 +0400

    String Methods updated

---
+++ Python_Fundamentals/str_methods.py
@@ -0,0 +1,19 @@
+#1
+#  word = 'Python'
+# a = word.center(50)
+# print(a)
+
+#2 
+# word = "python is very interesting web programming language."
+#x = word.capitalize()
+#print (x)
+
+#4 
+# word ="python - python is very interesting web programming language."
+# x = word.count("python")
+# print(x)
+
+#5
+# word = "Our earning is {price: .1f} dollars."
+# x =  (word.format(price=2000))
+# print (x)
