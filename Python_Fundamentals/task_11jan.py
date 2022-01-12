8d6ea7cf5b76441134d4cb899f2c9569941fd08b
Sevinj-Joy <seva.rasul15@gmail.com>
2022-01-12 20:34:57 +0400

    Python tasks updated

---
+++ Python_Fundamentals/task_11jan.py
@@ -0,0 +1,64 @@
+#task1
+#  a = (""" 2020-ci il üçün  ən yaxşı  
+# və innovativ “Proqramlaşdırma dilləri’’ni ən xırda detallarının belə diqqətinizdən yayınmağını 
+# istəmirsizə ekrana yaxın əyləşin və gözünüzü ‘’dörd ‘’açın :  Python, Java, C/C++,JavaScript,Go və ya
+#  Golang, R, Swift, PHP, C# """)
+# print ( a )
+
+
+#task2
+#  name = "Yusif"
+# surname ="Rehimli"
+# x= name.upper()
+# y= surname.upper()
+# print("Salam" , x , y)
+
+
+#task3
+# capitals = " Fransa,Praga,Baku,Istanbul,Warshava "
+# x = capitals.replace("," , "*")
+# print(x)
+
+#task4
+# words = "Macaristan"[::-1]
+# print(words)
+
+#task5
+# languages ="Python C JavaScript"
+# print("Languages:", languages )
+
+#task7
+#  x = 10
+# y = 55 
+# z = (x > y)
+# print(z)
+
+# x = 10
+# y = 55 
+# z = (x < y)
+# print(z)
+
+
+#task8
+
+#  import math
+# a = input("birincieded:")
+# b = input ("ikincieded:")
+# print(math.pow(int(a),int(b)))
+
+#task9
+# word = "culture"
+# senten_typle= "Nineteen Eighty-Four does not present art-as-culture"
+# x = senten_typle.find(word)
+# if "culture" in senten_typle:
+#     print("Metnde bu soz var")
+# else
+#     print("NOT FOUND")
+
+
+# if x == True :
+#      print( "Metnde bu soz var")
+#  elif x == False :
+#     print("NOT FOUND")
+
+# task10
