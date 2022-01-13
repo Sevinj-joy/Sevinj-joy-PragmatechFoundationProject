d10b741a02d42bbc0b6ab96b56f64a291b9af19f
Sevinj-Joy <seva.rasul15@gmail.com>
2022-01-13 19:54:15 +0400

    Task 11 jan updated

--- Python_Fundamentals/task_11jan.py
+++ Python_Fundamentals/task_11jan.py
@@ -47,20 +47,20 @@
 # print(math.pow(int(a),int(b)))
 
 #task9
-word = input("sozu daxil edin:")
-senten_typle = "Nineteen Eighty-Four does not present. art-as-culture"
-if "." in senten_typle:
-    n = senten_typle.split(".")
-if "-" in senten_typle:
-    t = senten_typle.split("-")
-if " " in senten_typle:
-    z = senten_typle.split(" ")
-y =n + t + z
+# word = input("sozu daxil edin:")
+# senten_typle = "Nineteen Eighty-Four does not present. art-as-culture"
+# if "." in senten_typle:
+#     n = senten_typle.split(".")
+# if "-" in senten_typle:
+#     t = senten_typle.split("-")
+# if " " in senten_typle:
+#     z = senten_typle.split(" ")
+# y =n + t + z
 
-if word in senten_typle:
-    print(f"Metnde {word} sozu var")
-else:
-    print(f"Metnde {word} sozu yoxdur")
+# if word in senten_typle:
+#     print(f"Metnde {word} sozu var")
+# else: 
+#     print(f"Metnde {word} sozu yoxdur")
 
 # task10
 # x = 65
---
+++ Python_Fundamentals/task_12jan.py
@@ -0,0 +1,44 @@
+# sual -Kvadrat yaratmaq olarsa daxil edilən 4 ədəddən, ekrana kvadratın sahəsini, olmazsa, həmin ədədlərin cəmini verin.
+# task1
+# a = int(input("Dordbucaqlinin enini daxil edin:"))
+# b = int(input("Dordbucaqlinin uzunlugunu daxil edin:"))
+# # perimetr = 2 * (a+b)
+# sahe_kvadrat = a ** 2
+# sahe_duzbucaqli = a * b
+# if a == b:
+#     print("Bu dordbucaqli kvadratdir ve sahesi:" , sahe_kvadrat)
+# else:
+#     print("Bu dordbucaqli kvadrat deyil ve sahesi:",sahe_duzbucaqli )
+
+
+# sual -Dörd ədəd daxil edin. Onlardan ən böyüyünü çapa verin.
+#task2
+# numbers = (22 , -18 , 0, 18)
+# maximum = max(numbers)
+# print("Ededlerden en boyuyu:", maximum)
+
+# sual -Proqram run olunanda, istifadəçiyə meyvələr menyusu təqdim olunsun. Userdən menyunuzdakı meyvələrdən birinin adını daxil etməsini tələb edin və ekrana meyvənin qiymətini yazdırın. (İstədiyiniz qiyməti yazdırın). Əgər menyuda olmayan meyvə daxil edilsə, ekrana error mesajı verin.
+# task3
+# meyveler = ["alma","armud ","banan","ananas","kivi","ciyelek"]
+# print ("Bu meyvelerden hansi meyveni isteyirsiniz?" , meyveler)
+# user_choosen = input("Meyveni daxil edin:")
+# # user_choosen = meyveler.index(input("Meyveni daxil edin:"))
+# qiymet = ["1kg = 3 AZN","1kg = 5 AZN", "1kg = 2 AZN","1kg = 4 AZN","1kg = 7 AZN","1kg = 14 AZN"]
+# if user_choosen == "alma":
+#     print(qiymet[0])
+# elif user_choosen == "armud":
+#     print(qiymet[1])
+# elif user_choosen == "banan":
+#     print(qiymet[2])
+# elif user_choosen == "ananas":
+#     print(qiymet[3])
+# elif user_choosen == "kivi":
+#     print(qiymet[4])
+# elif user_choosen == "ciyelek":
+#     print(qiymet[5])
+
+
+# task4
+name = input("Adinizi daxil edin:")
+surname =input ("Soyadinizi daxil edin:")
+age =input("Yasinizi daxil edin:")
