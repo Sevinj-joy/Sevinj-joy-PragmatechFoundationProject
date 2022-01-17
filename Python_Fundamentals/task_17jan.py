8d5e0b4fdc052a023ee7270922be9c0ef4cabdd9
Sevinj-Joy <seva.rasul15@gmail.com>
2022-01-17 20:39:12 +0400

    Task 17 jan updated

---
+++ Python_Fundamentals/task_17jan.py
@@ -0,0 +1,51 @@
+
+#task1 -Write a Python program to select the odd items of a list.
+
+# number= int(input("Enter a number: "))
+# if (number % 2) == 0:
+#    print("{0} is Even".format(number))
+# else:
+#    print("{0} is Odd".format(number))
+
+# task2_Write a Python function to sum all the numbers in a list.
+# import numbers
+
+
+# number_list =[]
+# user_num = int(input("Ne qeder eded daxil edilsin:"))
+# for i in range(user_num):
+#     numbers = int(input("Ededi daxil edin:"))
+#     number_list.append(numbers)
+# print("Sum of elements in given list is :", sum(number_list))
+
+
+
+# def topla():
+#     for i in range(user_num):
+#      numbers = int(input("Ededi daxil edin:"))
+#      number_list.append(numbers)
+
+# topla(i)
+
+# task3_Sample List : (8, 2, 3, 0, 7) Expected Output : 20
+# Sample_List = (8, 2, 3, 0, 7)
+# cem = 0
+# for x in Sample_List:
+#     cem +=x
+#     print("Yekun cem" , cem)
+
+# def sum_list(items):
+#     sum_numbers = 0
+#     for x in items:
+#         sum_numbers += x
+#     return sum_numbers
+# print(sum_list([78 , 4, 5 , 6]))
+
+#  task4_ Write a Python function to multiply all the numbers in a list.
+# list =(5 , 8 ,9, 1)
+# def multiply(list):
+#     yekun = 1
+#     for i in list:
+#         yekun = yekun * i
+#         return yekun
+# multiply()
