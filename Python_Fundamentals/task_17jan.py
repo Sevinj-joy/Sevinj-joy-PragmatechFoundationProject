cc9d7aaa2ff2ea174f4654fd5cba3d4440b47067
Sevinj-Joy <seva.rasul15@gmail.com>
2022-01-18 19:21:33 +0400

    Task 18 january updated

--- Python_Fundamentals/task_17jan.py
+++ Python_Fundamentals/task_17jan.py
@@ -48,4 +48,10 @@
 #     for i in list:
 #         yekun = yekun * i
 #         return yekun
-# multiply()
+# # multiply()
+# def find_max(nums):
+# max_num = float("-inf") # smaller than all other numbers
+# for num in nums:
+#    if num > max_num:
+# #  (Fill in the missing line here)
+#      return max_num
---
+++ Python_Fundamentals/task_18jan.py
@@ -0,0 +1,29 @@
+# task1 -Write a program to create a function show_employee() using the following conditions.
+# It should accept the employee’s name and salary and display both. If the salary is missing in the function
+# call then assign default value 9000 to salary
+# from os import name
+
+
+# def show_employer(name , salary = 12000):
+#     print(f"hello {name} your salary is: {salary} $")
+
+# name = input("Enter your name please:")
+# show_employer(name )
+
+# task2- 'test' adinda function yaradin, funksiyaya parametrleri key value shekline gonderin istenilen sayda, 
+# funksiya gonderilen ededlerden cut olanlarin sayini tapsin
+# def test(a,b,c):
+#     say =0
+#     if a % 2==0: #and b % 2 ==0 and c % 2==0:
+#         say = say + 1
+#     if b % 2 ==0:
+#         say = say + 1
+#     if  c % 2==0:
+#         say = say + 1
+#         print(f"{say} eded Cut eded daxil etdiniz ")
+#     else:
+#         print("Not found")
+# a =int(input("Enter first number ..."))
+# b =int(input("Enter second number ..."))
+# c =int(input("Enter second number ..."))
+# test(a , b, c)
