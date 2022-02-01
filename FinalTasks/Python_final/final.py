a4b329e9fa00debaec127c87b81ef7ac3daebb23
Sevinj-Joy <seva.rasul15@gmail.com>
2022-02-01 21:18:55 +0400

    Final task Python updated

---
+++ FinalTasks/Python_final/final.py
@@ -0,0 +1,20 @@
+# def outer(a ,b):
+#     def inner(c,d):
+#         return c+d
+#     return inner(a,b)
+#     return a
+# result =outer(5,10)
+# print(result)
+
+# yes =outer(5,10)
+# print(yes)
+
+# def fun(name ,age=20 ):
+#     print(name, age)
+# fun("emma" , 25)
+
+
+# def fun (**kwargs):
+#     for i in kwargs:
+#         print (i)
+# fun(em="kelly" ,salary=9000)
