1268d6b1cd223e02db33972d421759d343446dac
Sevinj-Joy <seva.rasul15@gmail.com>
2022-02-03 21:26:37 +0400

    Working

---
+++ FinalTasks/Python_final/Add_typle.py
@@ -0,0 +1,10 @@
+tpl=(23,45,12,67)
+
+def addToTuple(element):
+ for i in tpl:
+     i= i + 1
+ pass
+addToTuple(15)
+#  Tələblər
+# addToTuple metodunun vəzifəsi tpl strukturunun içinə yeni bir element əlavə edilə bilməsini təmin etməkdir.Metod icra olunduğu zaman nəticə olaraq yeni element əlavə edilmiş formada ekranda çap olunmalıdır.
+
