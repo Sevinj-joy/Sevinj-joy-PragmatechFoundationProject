02976073c8e99c82f27486a344ec5cfbe4dfcd16
Sevinj-Joy <seva.rasul15@gmail.com>
2022-02-07 11:20:23 +0400

    Change file name

--- FinalTasks/Python_final/Change_filename.py
+++ FinalTasks/Python_final/Change_filename.py
@@ -1,6 +1,10 @@
 #4.Masaüstü ya da hər hansı bir yerdə bir fayl mövcuddur.
 # Sizdən tələb olunan bu faylın adını dəyişdirən kod yazaraq linkini qeyd etməyinizdir.
 # İnput vasitəsi ilə sadəcə faylın olduğu folder və faylın adı daxil edilməlidir. 
-# Siz bu inputlara əsasən ad dəyişdirmə əməlyatını aparmalısınız
+# Siz bu inputlara əsasən ad dəyişdirmə əməlyatını 
+# import glob
+# import os
 
-input("Faylin adini daxil edin_")
+# File_name = "FinalTasks/old_name.txt"
+# Change_new_name= "FinalTasks/new.txt"
+# os.rename(File_name, Change_new_name)
